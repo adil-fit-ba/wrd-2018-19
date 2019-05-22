62ce5bc97d0e4e4607b2db29b0cd08664f57e2f7
Adil Joldić <adil@edu.fit.ba>
2019-05-23 01:40:55 +0200

    ok

---
+++ FIT Online shop/FIT Online shop/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FIT_Online_shop.EF;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIT_Online_shop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
