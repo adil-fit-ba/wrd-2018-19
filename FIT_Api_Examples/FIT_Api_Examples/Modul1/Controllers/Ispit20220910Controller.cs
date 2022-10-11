9f56c3ff923c4b7be8c376a196839c392a28aade
Adil <adil@fit.ba>
2022-10-12 00:45:15 +0200

    Revert "ok"
    This reverts commit 760193a1108a350ec194b0f26e957388f26e17fb.

--- FIT_Api_Examples/FIT_Api_Examples/Modul1/Controllers/Ispit20220910Controller.cs
+++ FIT_Api_Examples/FIT_Api_Examples/Modul1/Controllers/Ispit20220910Controller.cs
@@ -25,7 +25,7 @@ namespace FIT_Api_Examples.Modul1.Controllers
             public double CijenaDolar { get; set; }
             public string OpisPonude { get; set; }
             public List<string> Opcije { get; set; }
-            public string AkcijaPoruka { get; set; } = "";
+            public string AkcijaPoruka { get; set; }
         }
 
         private List<Destinacija2VM> listDestinacije => new List<Destinacija2VM>
@@ -105,7 +105,6 @@ namespace FIT_Api_Examples.Modul1.Controllers
                 CijenaDolar=5000,
                 OpisPonude="27.08.2022. - 8 dana - Hotel ***"     ,
                    Opcije = GetOpcije(),
-                   AkcijaPoruka = "Preostalo 1 mjesto",
                 ImageUrl="https://restapiexample.wrd.app.fit.ba/destinacije/box-offer-03.jpg"
             } ,
                         new Destinacija2VM
@@ -114,7 +113,6 @@ namespace FIT_Api_Examples.Modul1.Controllers
                 CijenaDolar=2000,
                 OpisPonude="21.09.2022. - 4 dana - Hotel ****"     ,
                    Opcije = GetOpcije(),
-                   AkcijaPoruka = "Popust $100 - danas",
                 ImageUrl="https://restapiexample.wrd.app.fit.ba/destinacije/box-offer-Mostar.jpg"
             } ,
                                     new Destinacija2VM
@@ -123,7 +121,6 @@ namespace FIT_Api_Examples.Modul1.Controllers
                 CijenaDolar=2000,
                 OpisPonude="04.07.2022. - 3 dana - Hotel ***"     ,
                    Opcije = GetOpcije(),
-                   AkcijaPoruka = "",
                 ImageUrl="https://restapiexample.wrd.app.fit.ba/destinacije/box-offer-05.jpg"
             } ,
                                                 new Destinacija2VM
@@ -132,7 +129,7 @@ namespace FIT_Api_Examples.Modul1.Controllers
                 CijenaDolar=5100,
                 OpisPonude="05.07.2022. - 5 dana - Hotel ***"     ,
                    Opcije = GetOpcije(),
-                   AkcijaPoruka = "Popust 35%",
+                   AkcijaPoruka = "",
                 ImageUrl="https://restapiexample.wrd.app.fit.ba/destinacije/box-offer-06.jpg"
             }
         };
