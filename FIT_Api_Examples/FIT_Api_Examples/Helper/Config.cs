bb79b7e5ce7266ec0f1e947aae0f2e46c4722d6b
Adil <adil@fit.ba>
2023-06-07 08:24:57 +0200

    2023-07

--- FIT_Api_Examples/FIT_Api_Examples/Helper/Config.cs
+++ FIT_Api_Examples/FIT_Api_Examples/Helper/Config.cs
@@ -7,7 +7,7 @@ namespace FIT_Api_Examples.Helper
 {
     public class Config
     {
-        public static string AplikacijURL = "https://wrd1.azurewebsites.net";
+        public static string AplikacijURL = "https://restapiexample.wrd.app.fit.ba";
 
         public static string Slike => "profile_images/";
         public static string SlikeURL => AplikacijURL + Slike;
