3408727e3e3363c21308c66fab451a7065468842
Adil <adil@fit.ba>
2023-10-11 10:13:50 +0200

    Update Ispit20230923Controller.cs
    dodao :

--- FIT_Api_Examples/FIT_Api_Examples/Modul1/Controllers/Ispit20230923Controller.cs
+++ FIT_Api_Examples/FIT_Api_Examples/Modul1/Controllers/Ispit20230923Controller.cs
@@ -59,7 +59,7 @@ namespace FIT_Api_Examples.Modul1.Controllers
                         .Select(pp => new
                         {
                             PrijeDana = (now - pp.DatumPolaska).Days,
-                            DatumPolaska = pp.DatumPolaska.ToString("dd.MM.yyyy HH-mm.sss"),//nije uredu
+                            DatumPolaska = pp.DatumPolaska.ToString("dd.MM.yyyy HH.mm"),//nije uredu
                         }).FirstOrDefault(),
 
                     NaredniPolazak = x.Ispit20230715PlaniranoPutovanjeList
